8a6239ff288831e9bae7ef680ab88b032a107c03
peibarthwu <peibarthwu@gmail.com>
2024-09-22 14:10:05 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ThreeScene  from './components/ThreeScene'
+
+function App() {
+  return (
+    <>
+      <ThreeScene/>
+    </>
+  )
+}
+
+export default App
